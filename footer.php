eebf2fae7a573953fdd5d11857ed9a81d5283916
feliww <felilailar@gmail.com>
2026-06-08 23:45:30 +0700

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-    </main>
-    <footer class="site-footer">
-        <p>GadgetStore &copy; <?= date('Y') ?>. Demo katalog smartphone dengan tampilan web e-commerce.</p>
-    </footer>
-</div>
-</body>
-</html>
